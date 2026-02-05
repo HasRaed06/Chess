48f353787f1c5701b5cf4c5af84394c6fd0c1b02
HasRaed06 <hasnaouiraed5@gmail.com>
2026-02-05 11:07:00 +0100

    a

--- chess.py
+++ chess.py
@@ -6,6 +6,7 @@ from PyQt6.QtGui import QIcon
 from PyQt6.QtCore import QSize
 import calc_moves
 import clicked
+import checks
 
 
 class ChessWindow(QMainWindow):
@@ -109,91 +110,12 @@ class ChessWindow(QMainWindow):
         else:
             return "white"
         return "black" if color == "w" else "white"
-
+    
     #checks if the color's king is on check or not
     def check(self,board,color):
-        for btn in self.findChildren(QPushButton):
-            if btn.property("piece") == color[0]+"k":
-                row,col = btn.property("board_pos")
-                color = board[row][col]['color']
-                break
-        #check for the pawn
-        coff = 1 if color == "black" else -1
-        print(row-coff)
-        print(col+1)
-        if 0<=row+coff<8:
-            if 0<=col+1<8:
-                piece1 = board[row+coff][col+1]
-            if piece1['color'] != color and piece1['type'] == 'pawn':
-                return True
+        return checks.check(self,board,color)
 
-            if 0<=col-1<8:
-                piece2 = board[row+coff][col-1]
-            if piece2['color'] != color and piece2['type'] == "pawn":
-                return True
-
-        direction1 = [
-                (1,0),
-                (0,1),
-                (-1,0),
-                (0,-1)
-        ]
-        direction2 = [
-                (1,1),
-                (-1,1),
-                (-1,-1),
-                (1,-1)
-        ]
-        direction3 = [
-            (1,2),
-            (-1,2),
-            (-2,1),
-            (-2,-1),
-            (-1,-2),
-            (1,-2),
-            (2,-1),
-            (2,1)
-        ]
-        #check diagnals
-        for dir in direction1:
-            rr,cc = dir
-            i = 1
-            r = row + rr*i
-            c = col + cc*i
-            while (0<=r<8 and 0<=c<8 and board[r][c]['type'] == ""):
-                i += 1
-                r = row + rr*i
-                c = col + cc*i
-            if 0<=r<8 and 0<=c<8:
-                piece = board[r][c]
-                if (piece['type'] == 'queen' or piece['type'] == 'rook') and piece['color'] != color:
-                    return True
-        #check for straight lines
-        for dir in direction2:
-            rr,cc = dir
-            i = 1
-            r = row + rr*i
-            c = col + cc*i
-            while (0<=r<8 and 0<=c<8 and board[r][c]['type'] == ""):
-                i += 1
-                r = row + rr*i
-                c = col + cc*i
-            if 0<=r<8 and 0<=c<8:
-                piece = board[r][c]
-                if (piece['type'] == 'queen' or piece['type'] == 'bishop') and piece['color'] != color:
-                    return True
-        #check for knight
-        for dir in direction3:
-            rr,cc = dir
-            r = row + rr
-            c = col + cc
-            if 0<=r<8 and 0<=c<8:
-                piece = board[r][c]
-                if piece['type'] == 'knight' and piece['color'] != color:
-                    return True
-        return False
- 
-
+    
     #checks if the game is over and return the winner(1/0/-1)
     def terminal(self,board):
         pass
