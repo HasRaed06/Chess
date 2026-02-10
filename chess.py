e547ea147ef93c5ff16b3a5f6be6b4b8210982f2
HasRaed06 <hasnaouiraed5@gmail.com>
2026-02-10 12:37:59 +0100

    aa

--- chess.py
+++ chess.py
@@ -8,54 +8,21 @@ import calc_moves
 import clicked
 import checks
 import copy
+import initialize
 
 class ChessWindow(QMainWindow):
     def __init__(self,board):
         super().__init__()
         self.initialize(board)
+        self.theme = ('green','tan','purple')
         loadUi("chess.ui", self)
         self.reset.clicked.connect(self.reset_game)
         self.display(board)
     
-    def initialize(self,board):
-        self.prv_pos = None
-        self.possible_moves = []
-        self.last_move = None
-        self.wking_moved = False
-        self.bking_moved = False
-        self.brq_moved = False
-        self.brk_moved = False
-        self.wrq_moved = False
-        self.wrk_moved = False
-        self.theme = ('green','tan','purple')
-        new_board = []
-        #initializing the board with dictionaries (type,color,image)
-        for line in range(8):
-            new_line = []
-            for piece in board[line]:
-                if piece:
-                    color = "black" if piece[0] == "b" else "white"
-                    type = piece[1]
-                    if type == "q":
-                        type = "queen"
-                    elif type == "k":
-                        type = "king"
-                    elif type == "b":
-                        type = "bishop"
-                    elif type == "n":
-                        type = "knight"
-                    elif type == "r":
-                        type = "rook"
-                    elif type == "p":
-                        type = "pawn"
-                    new_line.append({'type': type, 'color': color, 'image': 'images/'+color+type+'.jpg'})
-                else:
-                    new_line.append({'type': '', 'color': '', 'image': ''})
-            new_board.append(new_line)
-        
-        self.main_board = new_board
-
-
+    def initialize(self, board):
+        initialize.initialize(self, board)
+    
+    
     def display(self,board):
         #displaying the board in the UI
         for row in range(8):
